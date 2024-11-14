9187c9806a00a2475b4a94f89dcc8c91b35e6808
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-11-14 21:59:23 +0300

    Задача решена;

---
+++ stepik/beegeek/python_generation/course_01_for_beginners/modules/module_02/lesson_02/step_23.py
@@ -0,0 +1,21 @@
+"""
+14.11.2024
+"""
+
+def repeat_after_me(*args: str) -> tuple[str, ...]:
+    """
+    Notes:
+        Функция принимает на вход строки и возвращает их в том же порядке.
+    Args:
+        *args (str): Произвольное количество строк.
+    Returns:
+        (tuple[str, ...]): Строки в том же порядке.
+    """
+
+    return args
+
+
+if __name__ == "__main__":
+    words: list[str] = ["I was", "born", "this way"]
+
+    print(*repeat_after_me(*words), sep="\n")
