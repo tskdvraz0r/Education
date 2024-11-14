50eeb15c6bd9d73b16cff706d44bad210d2321e1
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-11-14 21:59:58 +0300

    Реализованы тесты для новых решений задач;

--- stepik/beegeek/python_generation/course_01_for_beginners/tests/tests_module_02/tests_lesson_02.py
+++ stepik/beegeek/python_generation/course_01_for_beginners/tests/tests_module_02/tests_lesson_02.py
@@ -12,6 +12,14 @@ from stepik.beegeek.python_generation.course_01_for_beginners.modules.module_02.
     lucky_sequence_2
 from stepik.beegeek.python_generation.course_01_for_beginners.modules.module_02.lesson_02.step_11 import \
     star_triangle
+from stepik.beegeek.python_generation.course_01_for_beginners.modules.module_02.lesson_02.step_21 import \
+    greetings
+from stepik.beegeek.python_generation.course_01_for_beginners.modules.module_02.lesson_02.step_22 import \
+    favorite_team
+from stepik.beegeek.python_generation.course_01_for_beginners.modules.module_02.lesson_02.step_23 import \
+    repeat_after_me
+from stepik.beegeek.python_generation.course_01_for_beginners.modules.module_02.lesson_02.step_24 import \
+    repeat_after_me_2
 
 
 @pytest.mark.parametrize(
@@ -43,3 +51,63 @@ def test_good_case_lucky_sequence(expected_result) -> None:
 )
 def test_good_case_star_triangle(expected_result) -> None:
     assert star_triangle() == expected_result
+
+
+@pytest.mark.parametrize(
+        ["test_name", "expected_result"],
+        [
+            ("Джек", "Привет, Джек"),
+            ("Джон", "Привет, Джон"),
+            ("Джули", "Привет, Джули")
+        ]
+)
+def test_good_case_greetings(
+        test_name,
+        expected_result
+) -> None:
+    assert greetings(name=test_name) == expected_result
+
+
+@pytest.mark.parametrize(
+        ["test_team_name", "expected_result"],
+        [
+            ("Динамо", "Динамо - чемпион!"),
+            ("СКА", "СКА - чемпион!"),
+            ("Slaughter to Prevail", "Slaughter to Prevail - чемпион!")
+        ]
+)
+def test_good_case_favorite_team(
+        test_team_name,
+        expected_result
+) -> None:
+    assert favorite_team(team_name=test_team_name) == expected_result
+
+
+@pytest.mark.parametrize(
+        ["test_args", "expected_result"],
+        [
+            (("I was", "born", "this way"), ("I was", "born", "this way")),
+            (("Let's", "start", "coding"), ("Let's", "start", "coding")),
+            (("I", "am", "a", "python", "programmer"), ("I", "am", "a", "python", "programmer"))
+        ]
+)
+def test_good_case_repeat_after_me(
+        test_args,
+        expected_result
+) -> None:
+    assert repeat_after_me(*test_args) == expected_result
+
+
+@pytest.mark.parametrize(
+        ["test_args", "expected_result"],
+        [
+            (("I was", "born", "this way"), ("this way", "born", "I was")),
+            (("Let's", "start", "coding"), ("coding", "start", "Let's")),
+            (("I", "am", "a", "python", "programmer"), ("programmer", "python", "a", "am", "I"))
+        ]
+)
+def test_good_case_repeat_after_me_2(
+        test_args,
+        expected_result
+) -> None:
+    assert repeat_after_me_2(*test_args) == expected_result
