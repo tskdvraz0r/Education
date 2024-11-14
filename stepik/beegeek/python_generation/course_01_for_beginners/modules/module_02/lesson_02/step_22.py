29082838b251baa4121812058d84ed3ed9c24094
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-11-14 21:59:19 +0300

    Задача решена;

---
+++ stepik/beegeek/python_generation/course_01_for_beginners/modules/module_02/lesson_02/step_22.py
@@ -0,0 +1,20 @@
+"""
+14.11.2024
+"""
+
+def favorite_team(team_name: str) -> str:
+    """
+    Notes:
+        Функция принимает на вход строку с наименованием команды и возвращает фразу:
+        f"{team_name} - чемпион!".
+    Args:
+        team_name (str): Наименование команды.
+    Returns:
+        (str): Строка: f"{team_name} - чемпион!".
+    """
+
+    return f"{team_name} - чемпион!"
+
+
+if __name__ == "__main__":
+    print(favorite_team(team_name="Зенит"))
